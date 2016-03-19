d1f587488a814cd4ba60f5db7cb5cf6874b1184f
Zlatko Fedor <zfedor@gmail.com>
2016-03-19 15:03:30 +0100

    first version

---
+++ src/action.js
@@ -0,0 +1,29 @@
+export default function(context, payload, done) {
+  context.executeAction(payload.action, payload.payload, (err, data) => {
+    if (err) {
+      context.dispatch('ERROR_HANDLE', {
+        error: err,
+      });
+
+      if (typeof payload.to === 'function') {
+        payload.to(err);
+      }
+
+      return done();
+    }
+
+    if (!payload.router || !payload.to) {
+      return done();
+    }
+
+    const to = typeof payload.to === 'function'
+      ? payload.to(null, data, payload)
+      : payload.to;
+
+    if (to) {
+      payload.router.push(to);
+    }
+
+    return done();
+  });
+}
