9ba90b5115f6feb3e268c10a9a9a410ea4b08df5
Zlatko Fedor <zfedor@gmail.com>
2016-06-23 22:57:40 +0200

    Release 1.0.6

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import { PropTypes } from 'react';
 import routerAction from './action';
-// import inherits from 'inherits';
-// import hoistNonReactStatics from 'hoist-non-react-statics';
 import createProvider from 'react-provide-props';
 
-const provider = createProvider('ExecuteActionWithRouterProvider', (props, context) => ({
+export default createProvider('ExecuteActionWithRouterProvider', (props, context) => ({
   executeAction: (action, payload, to) => context.executeAction(routerAction, {
     action,
     payload,
@@ -17,54 +15,3 @@ const provider = createProvider('ExecuteActionWithRouterProvider', (props, conte
   executeAction: PropTypes.func.isRequired,
   router: PropTypes.object.isRequired,
 });
-
-export default provider;
-/*
-function createComponent(CurrentComponent) {
-  function Provider(...args) {
-    Component.apply(this, args);
-
-    // bind
-    this.executeAction = ::this.executeAction;
-  }
-
-  inherits(Provider, Component);
-
-  const componentName = CurrentComponent.displayName || CurrentComponent.name;
-  Provider.displayName = componentName + 'ExecuteActionWithRouterProvider';
-  Provider.contextTypes = {
-    executeAction: PropTypes.func.isRequired,
-    router: PropTypes.object.isRequired,
-  };
-
-  Provider.prototype.executeAction = function executeActionMethod(action, payload, to) {
-    const { router, executeAction } = this.context;
-    executeAction(routerAction, {
-      action,
-      payload,
-      to,
-      router,
-    });
-  };
-
-  Provider.prototype.render = function render() {
-    return createElement(CurrentComponent, {
-      ...this.props,
-      executeAction: this.executeAction,
-    });
-  };
-
-  hoistNonReactStatics(Provider, CurrentComponent);
-
-  return Provider;
-}
-
-export default function provide(...args) {
-  // support decorator pattern
-  if (args.length === 0) {
-    return (ComponentToDecorate) => createComponent(ComponentToDecorate);
-  }
-
-  return createComponent.apply(null, args);
-}
-*/
